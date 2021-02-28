565a3bf6c7cf9e6dd35b3e5783bf754423ba9531
yasuyuki <godasper@gamil.com>
2021-02-28 09:26:13 +0900

    404not_foundエラーの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,20 +9,28 @@ class IpAdressRejected < ActionController::ActionControllerError; end
   rescue_from StandardError, with: :rescue500
   rescue_from Forbidden, with: :rescue403
   rescue_from IpAdressRejected, with: :rescue403
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
 
 
-  private def set_layout
+
+  private
+
+   def set_layout
     if params[:controller].match(%r{\A(staff)/})
       Regexp.last_match[1]
     end
   end
 
-  private def rescue403(e)
+  def rescue404(e)
+    render "errors/not_found", status: 404
+  end
+
+  def rescue403(e)
     @exception = e
     render "errors/forbidden", status: 403
   end
 
-  private def rescue500(e)
+  def rescue500(e)
     render "errors/internal_server_error", status: 500
   end
 
--- app/controllers/staff/top_controller.rb
+++ app/controllers/staff/top_controller.rb
@@ -1,5 +1,5 @@
 class Staff::TopController < ApplicationController
   def index 
-    raise Forbidden
+    
   end
 end
