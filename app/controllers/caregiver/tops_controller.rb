83f1eaab9979f34018108ac769a9d11137cd119e
yasuyuki <godasper@gamil.com>
2021-03-12 15:06:38 +0900

    詳細ページや削除機能の実施

--- app/controllers/caregiver/tops_controller.rb
+++ app/controllers/caregiver/tops_controller.rb
@@ -1,23 +1,34 @@
 class Caregiver::TopsController  < Caregiver::Base
+  protect_from_forgery :except => [:destroy]
 
   def index
-
+    @caregiver = Caregiver.find(params[:staff_member_id])
+    @care_recipitents = @caregiver.care_recipitents
   end
 
   def new
     @care_recipitent = CareRecipitent.new
   end
 
-  def show
-    @caregiver = Caregiver.find(params[:staff_member_id])
-    @care_recipitents = @caregiver.care_recipitents
-  end
-
   def create
     @care_recipitent = CareRecipitent.new(care_recipitent_params)  
     @care_recipitent.caregiver_id = session[:caregiver_id]
-    @care_recipitent.save
-    redirect_to caregiver_staff_member_top_path(params[:staff_member_id], @care_recipitent.id)
+   if @care_recipitent.save
+    redirect_to caregiver_staff_member_tops_path(params[:staff_member_id], @care_recipitent.id)
+   else
+    render :new
+   end
+
+  end
+
+  def show
+    @care_recipitent = CareRecipitent.find_by(id: params[:staff_member_id])
+  end
+
+  def destroy
+    @care_recipitent = CareRecipitent.find_by(id: params[:staff_member_id])
+    @care_recipitent.destroy
+    redirect_to caregiver_staff_member_tops_path(@care_recipitent.caregiver_id)
   end
 
   private
