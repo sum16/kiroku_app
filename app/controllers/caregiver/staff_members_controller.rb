1e5a85a4ad17f58c12ebad5c257e4ba2316aa5f4
yasuyuki <godasper@gamil.com>
2021-03-26 11:59:12 +0900

    スタッフ一覧画面に管理画面のリンクを追加

--- app/controllers/caregiver/staff_members_controller.rb
+++ app/controllers/caregiver/staff_members_controller.rb
@@ -5,6 +5,7 @@ class Caregiver::StaffMembersController < Caregiver::Base
   def index
     @caregivers = Caregiver.order(created_at: :desc)
     @caregivers_paginate = @caregivers.page(params[:page])
+    @caregiver = Caregiver.find_by(id: current_caregiver_member.id)
     
   end
 
