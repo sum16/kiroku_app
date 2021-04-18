dd5af4d577504cd68a41d9b31339df671a77c219
yasuyuki <godasper@gamil.com>
2021-04-18 23:25:38 +0900

    確認ボタンの修正

--- app/controllers/caregiver/share_buttons_controller.rb
+++ app/controllers/caregiver/share_buttons_controller.rb
@@ -1,20 +1,13 @@
 class Caregiver::ShareButtonsController < Caregiver::Base
-  
-  def index
-  end
-
   def create
     @share_button = current_caregiver_member.share_buttons.create(post_id: params[:post_id])
-    if @share_button.save!
-       redirect_to caregiver_posts_path
-    end
+
   end
 
   def destroy
     post = Post.find(params[:post_id])
     share_button = current_caregiver_member.share_buttons.find_by(post_id: post.id)
     share_button.destroy
-    redirect_to caregiver_posts_path
   end
 
 end
