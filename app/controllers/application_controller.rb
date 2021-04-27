bdf09e93181b2644109879618a5c3f40a8be8f65
yasuyuki <godasper@gamil.com>
2021-04-27 10:42:29 +0900

    エラー処理の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,19 +17,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def rescue404(e)
-    render "errors/not_found", status: 404
-  end
-
-  def rescue403(e)
-    @exception = e
-    render "errors/forbidden", status: 403
-  end
-
-  def rescue500(e)
-    render "errors/internal_server_error", status: 500
-  end
-
   #xhrメソッド = ajaxによるリクエストかどうかを判別
   def  reject_non_xhr
     raise ActionController::BadRequest unless request.xhr?
